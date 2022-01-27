61564e38a2c065852e7c55dd6e4ead1c11c53d0e
Dimitri C. IFR <65430590+dimitribzh@users.noreply.github.com>
2022-01-27 11:37:54 +0400

    Update notation.js

--- notation.js
+++ notation.js
@@ -11,28 +11,39 @@ const url2 = `https://fr.indeed.com/companies/search?q=${requete}&l=france`;
 const src1 = 'work/src1.pdf';
 const src2 = 'work/src2.pdf';
 
-//Téléchargement des sources avec la méthode Puppeteer
-(async () => {
+//Scraping avec la méthode Puppeteer
+const getData = async () => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
   await page.goto(url1);
-  await page.pdf({ path: src1 });
+  const result = await page.evaluate(() => {
+    let note1 = document.querySelector("#app > div.container.scope-all.loading-false > section.results.count-1 > div > div > a.rating > span.all").innerText;
+    return note1
+  })
   await browser.close();
-})();
+  let note1 = result / 5
+  return note1
+}
 
-(async () => {
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-    await page.goto(url2);
-    await page.pdf({ path: src2 });
-    await browser.close();
-  })();
+//Affichage 
+getData().then(value => {
+  console.log(`Score moral : ${value} / 20`)
+})
 
-//Extraction des sources
+//
+const getData2 = async () => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto(url2);
+  const result = await page.evaluate(() => {
+    let note = document.querySelector("#main > div > div.css-1aehwuu-Box.eu4oa1w0 > section > div:nth-child(2) > div.css-7vf1t1-Flex.e37uo190 > div > div.css-1tmuurb-Box.eu4oa1w0 > div:nth-child(2) > a > div > span").innerText;
+    return note
+  })
+  await browser.close();
+  let note = result * 4
+  return note
+}
 
-
-//Calcul
-
-//Affichage
-
-//Nettoyage
+getData2().then(value2 => {
+  console.log(`Condition de travail : ${value2} / 20`)
+})
